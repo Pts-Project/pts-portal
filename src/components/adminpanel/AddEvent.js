114f884776c6bc34a87fc1286b6f262a5fe02686
MD ARIF KHAN <arifthehappy@gmail.com>
2021-02-01 00:31:24 +0530

    events added

--- src/components/adminpanel/AddEvent.js
+++ src/components/adminpanel/AddEvent.js
@@ -1,4 +1,4 @@
-import React,{Component,useState} from 'react'
+import React, { Component, useState } from 'react'
 import './AddEvent.css'
 import axios from 'axios'
 import { Router, Route, Link } from 'react-router-dom';
@@ -6,74 +6,74 @@ import { createBrowserHistory } from 'history';
 
 import { useHistory } from 'react-router-dom'
 
-function AddEvent(){
-    const [SelImage,setSelImage]= useState("");
-    const [ImgName,setImgName]= useState("");
+function AddEvent() {
+    const [SelImage, setSelImage] = useState("");
+    const [ImgName, setImgName] = useState("");
 
-    const uploadImage=(files)=>{
-     
-        const formdata=new FormData()
-        formdata.append('file',SelImage)
-        formdata.append('upload_preset','platform')
-        axios.post('https://api.cloudinary.com/v1_1/djxi7xjop/image/upload',formdata)
-            .then(res=>{
-            
+    const uploadImage = (files) => {
+
+        const formdata = new FormData()
+        formdata.append('file', SelImage)
+        formdata.append('upload_preset', 'platform')
+        axios.post('https://api.cloudinary.com/v1_1/djxi7xjop/image/upload', formdata)
+            .then(res => {
+
                 console.log(res.data.secure_url)
-                const url=res.data.secure_url
+                const url = res.data.secure_url
                 console.log(ImgName)
-                const cloudata={
-                    image:url,
-                    name:ImgName
+                const cloudata = {
+                    image: url,
+                    name: ImgName
                 }
-                axios.post('http://localhost:5000/event/create',cloudata)
-                .then(res=>{                
-          
-                })
-                .catch(err=>{
-                    console.log(err)
-                })
-                
-             })
-            .catch(err=>{
+                axios.post('/event/create', cloudata)
+                    .then(res => {
+
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+
+            })
+            .catch(err => {
                 console.log(err)
             })
-            
+
     };
 
-    return(
+    return (
         <div>
-        <section className="notlogin">
-            <div className="lContainer">
-                <div class="heading">
-                    Add Event
+            <section className="notlogin">
+                <div className="lContainer">
+                    <div class="heading">
+                        Add Event
                 </div>
-                <label>
-                    Event Name
-                </label>
-                <input 
-                    type="text"
-                    value={ImgName}
-                    required
-                    onChange={(e)=>{setImgName(e.target.value)}}
-                />
-                <div className="pass">
                     <label>
-                        Upload Image from Device
-                    </label>
-                    <input 
-                        type="file"
-                        autoFocus
+                        Event Name
+                </label>
+                    <input
+                        type="text"
+                        value={ImgName}
                         required
-               
-                        onChange={(e)=>{setSelImage(e.target.files[0])}}
+                        onChange={(e) => { setImgName(e.target.value) }}
                     />
+                    <div className="pass">
+                        <label>
+                            Upload Image from Device
+                    </label>
+                        <input
+                            type="file"
+                            autoFocus
+                            required
+
+                            onChange={(e) => { setSelImage(e.target.files[0]) }}
+                        />
+                    </div>
+                    <div className="lbtnContainer">
+                        <button className="lbtn" onClick={uploadImage} >Add</button>
+                    </div>
                 </div>
-                <div className="lbtnContainer">
-                    <button className="lbtn"onClick={uploadImage} >Add</button>
-                </div>
-            </div>           
-        </section>
-    </div>
+            </section>
+        </div>
     );
 }
 export default AddEvent;
--- src/components/dashboard/Home.js
+++ src/components/dashboard/Home.js
@@ -18,119 +18,119 @@ class Home extends Component {
         return (
             <div className="main">
                 <div className="hcontainer">
-                    
-        <Particles id="particles-js"
-            params={{
-                "particles": {
-                    "number": {
-                        "value": 60,
-                        "density": {
-                            "enable": true,
-                            "value_area": 800
-                        }
-                    },
-                    "color": {
-                        "value": "#FFFFFF"
-                    },
-                    "shape": {
-                        "type": "circle",
-                        "stroke": {
-                            "width": 0,
-                            "color": "#000000"
-                        },
-                        "polygon": {
-                            "nb_sides": 7
-                        },
-                        "image": {
-                            "src": "img/github.svg",
-                            "width": 100,
-                            "height": 100
-                        }
-                    },
-                    "opacity": {
-                        "value": 1,
-                        "random": true,
-                        "anim": {
-                            "enable": false,
-                            "speed": 1,
-                            "opacity_min": 0.1,
-                            "sync": false
-                        }
-                    },
-                    "size": {
-                        "value": 3,
-                        "random": true,
-                        "anim": {
-                            "enable": false,
-                            "speed": 40,
-                            "size_min": 0.1,
-                            "sync": false
-                        }
-                    },
-                    "line_linked": {
-                        "enable": true,
-                        "distance": 150,
-                        "color": "#FFFFFF",
-                        "opacity": 0.5,
-                        "width": 1
-                    },
-                    "move": {
-                        "enable": true,
-                        "speed": 6,
-                        "direction": "none",
-                        "random": false,
-                        "straight": false,
-                        "out_mode": "out",
-                        "bounce": false,
-                        "attract": {
-                            "enable": false,
-                            "rotateX": 600,
-                            "rotateY": 1200
-                        }
-                    }
-                },
-                "interactivity": {
-                    "detect_on": "canvas",
-                    "events": {
-                        "onhover": {
-                            "enable": true,
-                            "mode": "repulse"
-                        },
-                        "onclick": {
-                            "enable": true,
-                            "mode": "remove"
-                        },
-                        "resize": true
-                    },
-                    "modes": {
-                        "grab": {
-                            "distance": 200,
-                            "line_linked": {
-                                "opacity": 1
-                            }
-                        },
-                        "bubble": {
-                            "distance": 200,
-                            "size": 60,
-                            "duration": 2,
-                            "opacity": 4,
-                            "speed": 3
-                        },
-                        "repulse": {
-                            "distance": 200,
-                            "duration": 0.8
-                        },
-                        "push": {
-                            "particles_nb": 5
-                        },
-                        "remove": {
-                            "particles_nb": 1
-                        }
-                    }
-                },
-                "retina_detect": false
-            }}
-        />
+
+                    <Particles id="particles-js"
+                        params={{
+                            "particles": {
+                                "number": {
+                                    "value": 60,
+                                    "density": {
+                                        "enable": true,
+                                        "value_area": 800
+                                    }
+                                },
+                                "color": {
+                                    "value": "#FFFFFF"
+                                },
+                                "shape": {
+                                    "type": "circle",
+                                    "stroke": {
+                                        "width": 0,
+                                        "color": "#000000"
+                                    },
+                                    "polygon": {
+                                        "nb_sides": 7
+                                    },
+                                    "image": {
+                                        "src": "img/github.svg",
+                                        "width": 100,
+                                        "height": 100
+                                    }
+                                },
+                                "opacity": {
+                                    "value": 1,
+                                    "random": true,
+                                    "anim": {
+                                        "enable": false,
+                                        "speed": 1,
+                                        "opacity_min": 0.1,
+                                        "sync": false
+                                    }
+                                },
+                                "size": {
+                                    "value": 3,
+                                    "random": true,
+                                    "anim": {
+                                        "enable": false,
+                                        "speed": 40,
+                                        "size_min": 0.1,
+                                        "sync": false
+                                    }
+                                },
+                                "line_linked": {
+                                    "enable": true,
+                                    "distance": 150,
+                                    "color": "#FFFFFF",
+                                    "opacity": 0.5,
+                                    "width": 1
+                                },
+                                "move": {
+                                    "enable": true,
+                                    "speed": 6,
+                                    "direction": "none",
+                                    "random": false,
+                                    "straight": false,
+                                    "out_mode": "out",
+                                    "bounce": false,
+                                    "attract": {
+                                        "enable": false,
+                                        "rotateX": 600,
+                                        "rotateY": 1200
+                                    }
+                                }
+                            },
+                            "interactivity": {
+                                "detect_on": "canvas",
+                                "events": {
+                                    "onhover": {
+                                        "enable": true,
+                                        "mode": "repulse"
+                                    },
+                                    "onclick": {
+                                        "enable": true,
+                                        "mode": "remove"
+                                    },
+                                    "resize": true
+                                },
+                                "modes": {
+                                    "grab": {
+                                        "distance": 200,
+                                        "line_linked": {
+                                            "opacity": 1
+                                        }
+                                    },
+                                    "bubble": {
+                                        "distance": 200,
+                                        "size": 60,
+                                        "duration": 2,
+                                        "opacity": 4,
+                                        "speed": 3
+                                    },
+                                    "repulse": {
+                                        "distance": 200,
+                                        "duration": 0.8
+                                    },
+                                    "push": {
+                                        "particles_nb": 5
+                                    },
+                                    "remove": {
+                                        "particles_nb": 1
+                                    }
+                                }
+                            },
+                            "retina_detect": false
+                        }}
+                    />
                     <div id="overlay">
                         <h1 className="title">PLATFORM TECH SOLUTION</h1>
                         <p>
@@ -143,15 +143,15 @@ class Home extends Component {
                     <About />
                 </div>
 
-     
+                <div className="Achieve">
+                    <Achive />
+                </div>
+
 
                 <div className="Departments">
                     <Departments />
                 </div>
-                <div className="Achieve">
-                    <Achive />
-                </div>
-     
+
 
                 <div className="bottomContent">
                     <BottomContent />
