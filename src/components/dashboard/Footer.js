a6f1e176f256ab90a37f41e5dd93c9dd1d58c15b
MD ARIF KHAN <arifthehappy@gmail.com>
2021-03-14 10:29:07 +0530

    footeralign

--- src/components/dashboard/Footer.js
+++ src/components/dashboard/Footer.js
@@ -1,51 +1,8 @@
 import './footer.css';
-import SimpleReactFooter from "simple-react-footer";
+//import SimpleReactFooter from "simple-react-footer";
 import { Container, Row, Col } from 'react-grid-system';
 
 
-
-const description = "We help our members succeed by providing mentorships. \n Patform is here to help you get industry ready, via student engaged skill development along with team management.";
-const title = "Platform Tech Club";
-const columns = [
-    {
-        title: "Links",
-        resources: [
-            {
-                name: "About",
-                link: "#about"
-            },
-            {
-                name: "Careers",
-                link: "/careers"
-            },
-            {
-                name: "Events",
-                link: "/event"
-            },
-            {
-                name: "Admin",
-                link: "/admin"
-            }
-        ]
-    },
-    {
-        title: "Reach Us",
-        resources: [
-            {
-                name: "Panjab University SSG Regional Center ",
-                link: "/locations"
-            },
-            {
-                name: "Hoshiarpur,India,146023",
-                link: "/locations"
-            },
-            {
-                name: "student@platformtechs.in",
-                link: "mailto:student@platformtechs.in"
-            }
-        ]
-    }
-];
 
 
 
@@ -55,30 +12,58 @@ const Footer = () => {
             <div className="footerContent">
                 <Container fluid>
                     <Row>
-                        <Col md={12}>
-                            <SimpleReactFooter className="hovera"
-
-                                description={description}
-                                title={title}
-                                columns={columns}
-                                facebook="platform.techs.31"
-                                twitter="platformtechs"
-                                instagram="page2platform"
-                                linkedin="platform-tech-solutions-086323197"
-                                pinterest=""
-                                youtube=""
-                                copyright="PlatformTechSolution | All Rights Reserved"
-                                iconColor="white"
-                                backgroundColor="#302e2ece"
-                                fontColor="lightcyan"
-                                copyrightColor="darkgrey"
-                            />;
+                        <Col md={8} sm={12}>
+                            <h4>Platform Tech Group</h4>
+                            <p>"We help our members succeed by  providing mentorship  Platform is here to help you get industry ready, via student engaged skill development along with team management"</p>
+                        </Col>
+                        <Col md={1}>
+                            <h5>Links</h5>
+                            <ul>
+                                <a href="#About"> <li> About</li></a>
+                                <a href="/careers"><li> Careers</li></a>
+                                <a href="/events"><li> Events</li></a>
+                                <a href="/admin"><li> Admin</li></a>
+                            </ul>
+                        </Col>
+                        <Col md={3}>
+                            <h5>Reach Us</h5>
+                            <ul>
+                                <p>
+                                    <a href="https://goo.gl/maps/vo9jhJGraN72p2Sv9">
+                                        <p>Panjab University SSG Regional Centre <br></br>
+                                        Hoshiarpur,Punjab , India, 146023
+                                        </p>
+                                    </a>
+                                    <a href="mailto:student@platformtechs.in">
+                                        <p>student@platformtechs.in</p>
+                                    </a>
+                                </p>
+                            </ul>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={12}><h6 align="center" color="aliceblue"> Stay Connected</h6></Col>
+                    </Row>
+                    <Row>
+                        <Col md={12} align="center">
+                            <social class="social" align="center">
+                                <a href="fb.com"> <fab class="fb">Facebook</fab></a>&nbsp;
+                                <a href="fb.com"> <fab class="twt">Twitter</fab></a>&nbsp;
+                                <a href="fb.com"> <fab class="insta">Instagram</fab></a>&nbsp;
+                                <a href="fb.com"> <fab class="lnkd">Linkedin</fab></a>
+                            </social>
+                        </Col>
+                    </Row>
+                    <br></br>
+                    <Row>
+                        <Col md={12} align="center" id="copyright">
+                            <p>Copyright @copy 2021 Platform Techs | All Rights Reserved</p>
                         </Col>
                     </Row>
                 </Container>
 
             </div>
-        </div>
+        </div >
     );
 }
 
