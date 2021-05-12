35c1df07a2923b3ffad0d10180abe50b1d481090
tanmyavishvakarma <tanmya2000@gmail.com>
2021-05-12 15:38:06 +0530

    navabar issue2

--- src/components/dashboard/Navbar.js
+++ src/components/dashboard/Navbar.js
@@ -1,44 +1,63 @@
 import { Component } from "react"
 import React from 'react'
 import { Link } from "react-router-dom";
-import {Icon} from 'react-materialize';
-import './navbar.css';
-import SignInLinks from './signlinks/signedinlinks'
-import SignOutLinks from './signlinks/signedoutlinks'
-class Nav extends Component{
-    render(){
-        const val=localStorage.getItem('test')
-        var links;
-        if(val==="a"){
-            links=<SignOutLinks></SignOutLinks>
-        }else{
-            links=<SignInLinks></SignInLinks>
+import { Icon, SideNav } from 'react-materialize';
+import './navbar.css'
+import Logo from '../assets/ptslogo.png'
+import SignedInLinks from './signlinks/signedinlinks'
+import SignedOutLinks from './signlinks/signedoutlinks'
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            links: '',
+            status: 'top'
         }
-        return(
+    }
+    componentDidMount() {
+        this.listener = document.addEventListener("scroll", e => {
+            var scrolled = document.scrollingElement.scrollTop;
+            if (scrolled >= 290) {
+                if (this.state.status !== "boottm") {
+                    this.setState({ status: "bottom" });
+                }
+            } else {
+                if (this.state.status !== "top") {
+                    this.setState({ status: "top" });
+                }
+            }
+        });
+    }
 
-            <div>
-                <nav>
-                    <div className="nav-wrapper black">
-                        <Link to="/"><span className="logo left">JobPortal</span></Link>
+    componentDidUpdate() {
+        document.removeEventListener("scroll", this.listener);
+    }
+
+    render() {
+        const val = localStorage.getItem("test")
+        if (val === 'a') {
+            var links = <SignedOutLinks></SignedOutLinks>
+        } else {
+
+            var links = <SignedInLinks></SignedInLinks>
+
+        }
+        return (
+            <div className="navbar">
+                <nav class="transparent z-depth-0">
+                    <div className="nav-wrapper" style={{ backgroundColor: this.state.status === "top" ? "rgba(10, 10, 10, 0.900)" : "black" }}>
+                        <Link to="/"><div className="logo left"><img className="logoimg" src={Logo} width="64px" alt="Platform Tech Solutions"></img></div></Link>
                         <span className="sidenav-trigger hide-on-large-only right" data-target="slide-out"><Icon>menu</Icon></span>
-                            <ul id="nav-mobile" className="right hide-on-med-and-down">
-                                {/* <li><Link className="menu1 white-text" to="/findJob">Find A Job</Link></li>
-                                <li><Link className="white-text" to="/postJob">Post A Job</Link></li>
-                                <li><Link className="white-text" to="/login">Login</Link></li>
-                                <li><Link className="white-text"  to="/register">Register</Link></li> */}
-                                {links}
-                            </ul>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
+                            {links}
+                        </ul>
                     </div>
                 </nav>
                 <ul className="sidenav black" id="slide-out">
-                    {/* <li><Link className="sidenav-close white-text" to="/findJob">Find a Job</Link></li>
-                    <li><Link className="sidenav-close white-text" to="/postJob">Post a Job</Link></li>
-                    <li><Link className="sidenav-close white-text" to="/login">Login</Link></li>
-                    <li><Link className="sidenav-close white-text" to="/register">Register</Link></li> */}
                     {links}
                 </ul>
-        </div>
+            </div>
         );
     }
 }
-export default Nav;
+export default Navbar;
